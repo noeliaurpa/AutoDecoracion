e409b8097e6bef7ade03e40bb0b09a55311276d4
Noelia Urpí P <noeurpa1114@hotmail.com>
2017-08-19 15:01:02 -0600

    Change in the notifications of laravel in spanish
    Cambios en las notificaciones de laravel porque por defecto vienen en
    ingles y se necesitaban en español.

--- app/Http/Controllers/Sms.php
+++ app/Http/Controllers/Sms.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Redirect;
 use App\Customer;
 use App\Notification;
 use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Validator;
 
 class Sms extends Controller
 {
@@ -23,28 +24,38 @@ class Sms extends Controller
     public function send(Request $request)
     {
         try {
-            $api = new TextMagicAPI(array
+            $validator = Validator::make($request->all(),[
+                'message' => 'required|string|max:70',
+                'tellClient' => 'required|integer|max:99999999'
+                ]);
+
+            if($validator->fails()){
+                Session::flash('flash_message', 'Algunos de los campos se insertaron de forma incorrecta, al final podrá observar el error');
+                return redirect('send')->withErrors($validator)->withInput();
+            }else{
+                $api = new TextMagicAPI(array
+                    (
+                        'username' => 'noeliaurpa',
+                        'password' => 'Noelia111495'
+                        )
+                    );
+
+                $text = $request->message;
+
+                $numero = (float)$request->tellClient;
+
+                $phones = array
                 (
-                    'username' => 'noeliaurpa',
-                    'password' => 'Noelia111495'
-                    )
-                );
-
-            $text = $request->message;
-
-            $numero = (float)$request->tellClient;
-
-            $phones = array
-            (
             // Country Number + Phone Number
             $numero,  // Phone1
             );
 
-            $is_unicode = true;
+                $is_unicode = true;
 
-            $api->send($text, $phones, $is_unicode);
-            Session::flash('success_message','Se envió el mensaje correctamente.');
-            return Redirect::to('/message');
+                $api->send($text, $phones, $is_unicode);
+                Session::flash('success_message','Se envió el mensaje correctamente.');
+                return Redirect::to('/message');
+            }
         } catch (\Exception $e) {
             Session::flash('flash_message','No se puede enviar mensajes porque no tienes saldo suficiente. '.$e);
             return Redirect::to('/message');
